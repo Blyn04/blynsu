7665866949178336969a2a6f2b8b119417ee22ec
berlene <berlenebernabe12@gmail.com>
2025-07-02 12:35:43 +0800

    TEST 4

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html',
+      template: './src/index.html',
       favicon: `./src/assets/fav.png`,
     }),
   ],
