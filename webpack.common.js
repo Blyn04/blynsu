9aa553c464990cc9e20e77f9be53cac8cc2b4b57
berlene <berlenebernabe12@gmail.com>
2025-07-14 20:31:40 +0800

    WEBPACK REVERTED

--- webpack.common.js
+++ webpack.common.js
@@ -1,68 +1,28 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   entry: './index.js',
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './index.html',
-//       favicon: `./src/assets/fav.png`,
-//     }),
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.html$/,
-//         use: ['html-loader'],
-//       },
-//       {
-//         test: /\.(svg|png|jpg|jpeg|gif)$/i,
-//         use: {
-//           loader: 'file-loader',
-//           options: {
-//             esModule: false,
-//             name: '[name].[hash].[ext]',
-//             outputPath: 'imgs',
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
-
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+  entry: './index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // ✅ root-level index.html is fine
-      favicon: './src/assets/fav.png',
+      template: './index.html',
+      favicon: `./src/assets/fav.png`,
     }),
   ],
   module: {
     rules: [
-      // ✅ REMOVE THIS unless you use <%- require() %> in .html
-      // {
-      //   test: /\.html$/,
-      //   use: ['html-loader'],
-      // },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.html$/,
+        use: ['html-loader'],
       },
       {
         test: /\.(svg|png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'imgs/[name].[hash][ext]',
+        use: {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
         },
       },
     ],
