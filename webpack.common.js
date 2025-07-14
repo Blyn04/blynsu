7a7320cf6c108dd45e3862c693f30f1f8e5675a7
berlene <berlenebernabe12@gmail.com>
2025-07-14 20:29:13 +0800

    TEST WEBPACK

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,50 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './index.js',
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './index.html',
+//       favicon: `./src/assets/fav.png`,
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.html$/,
+//         use: ['html-loader'],
+//       },
+//       {
+//         test: /\.(svg|png|jpg|jpeg|gif)$/i,
+//         use: {
+//           loader: 'file-loader',
+//           options: {
+//             esModule: false,
+//             name: '[name].[hash].[ext]',
+//             outputPath: 'imgs',
+//           },
+//         },
+//       },
+//     ],
+//   },
+// };
+
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  mode: 'production', // or 'development'
+  entry: './src/index.js', // ✅ keep pointing to src/index.js
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
-      favicon: `./src/assets/fav.png`,
+      template: './index.html', // ✅ uses index.html from root
+      favicon: './src/assets/fav.png',
     }),
   ],
   module: {
@@ -16,15 +55,14 @@ module.exports = {
       },
       {
         test: /\.(svg|png|jpg|jpeg|gif)$/i,
-        use: {
-          loader: 'file-loader',
-          options: {
-            esModule: false,
-            name: '[name].[hash].[ext]',
-            outputPath: 'imgs',
-          },
+        type: 'asset/resource', // ✅ better than file-loader in Webpack 5
+        generator: {
+          filename: 'imgs/[name].[hash][ext]',
         },
       },
     ],
+  },
+  resolve: {
+    extensions: ['.js'],
   },
 };
